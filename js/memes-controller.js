2b58d60538dec3fd5f0220c54e877c0da2c8969e
eliyahu-dayan-1 <dayan0544@gmail.com>
2020-04-12 04:57:29 +0300

    My 18th commit!

--- js/memes-controller.js
+++ js/memes-controller.js
@@ -247,11 +247,11 @@ function renderBitImg(images) {
     images.forEach(image => {
         var elImg = `<div><img class="my-meme-image" src=${image.url}
         alt="">
-        <button onclick="onEditSavedMeme(${image.id})" class="memes-edit"><img class="operation-img" src="/img/icons/edit-icon.svg"
+        <button onclick="onEditSavedMeme(${image.id})" class="memes-edit"><img class="operation-img" src="img/icons/edit-icon.svg"
         alt="edit"></button>
-        <a href="#" class="memes-download" onclick="onDownloadSavedMeme(this, ${image.id})" download="my-img.jpg" ><img class="operation-img" src="/img/icons/download-icon.png"
+        <a href="#" class="memes-download" onclick="onDownloadSavedMeme(this, ${image.id})" download="my-img.jpg" ><img class="operation-img" src="img/icons/download-icon.png"
         alt="download"></a>
-            <button onclick="onEarseSavedMeme(${image.id})" class="memes-earse"><img class="operation-img" src="/img/icons/trash.png"
+            <button onclick="onEarseSavedMeme(${image.id})" class="memes-earse"><img class="operation-img" src="img/icons/trash.png"
             alt="delete text"></button>
             </div>
             `;
